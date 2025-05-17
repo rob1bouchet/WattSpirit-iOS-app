b41660483a590929cb1cb5765be04fca9b204cd1
rob1bouchet <rbouchet@newtone.com>
2025-05-17 23:08:17 +0200

    Initial Commit

---
+++ WattSpiritiOS/WattSpiritiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattSpiritiOSApp.swift
+//  WattSpiritiOS
+//
+//  Created by Robin Bouchet on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WattSpiritiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
